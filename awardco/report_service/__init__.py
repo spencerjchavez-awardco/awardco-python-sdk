d87ff52e661865a2e6867b88000387653a0a61b4
Spencer Chavez <spencer.chavez@awardco.com>
2026-03-06 10:49:46 -0700

    update report request schema

--- awardco/report_service/__init__.py
+++ awardco/report_service/__init__.py
@@ -1,3 +1,4 @@
 from .report_service import ReportService
-from .report_request import ReportRequest, ReportFilters, TimeRangeOption, ReportId
+from .report_request import ReportRequest, ReportFilters, TimeRangeOption
+from .report_id import ReportId
 from .timezone import Timezone
